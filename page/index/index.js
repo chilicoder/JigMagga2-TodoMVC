cf5c33c5b6ed2d7c8e85f664ecaed7093e8d6cb7
Jaroslav.Popov <jaroslav.popov@takeaway.com>
2015-07-31 12:54:17 +0200

    merge

--- page/index/index.js
+++ page/index/index.js
@@ -1,11 +1,6 @@
 var Magga = require("magga").getInstance(),
-<<<<<<< HEAD
-	// todo rename into MaggaMediator
-	Mediator = new (require("magga-mediator"))()
-=======
 // todo rename into MaggaMediator
 	Mediator = new (require("magga-mediator"))();
->>>>>>> 7ad3d2b5939a72b39c4bbe50021a753d109376c6
 
 // todo use extension API
 Magga.Mediator = Mediator;
